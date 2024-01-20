3196571d31c19fe31d0cd29b5514f7d2e52fdde7
Yulia <123819378+Yulia8668@users.noreply.github.com>
2024-01-20 23:26:16 +0000

    Add files via upload

---
+++ product_list.js
@@ -0,0 +1,100 @@
+const PRODUCT_LIST = [
+    {
+        id: 'pr1',
+        name: 'Снеговичок декоративный',
+        img: 'images/product_list/pr.1.1.jpg',
+        price: 7,
+    },
+    {
+        id: 'pr2',
+        name: 'Олененок декоративный',
+        img: 'images/product_list/pr.1.2.jpg',
+        price: 4.50,
+        discount: '10%'
+    },
+    {
+        id: 'pr3',
+        name: 'Салфетки льняные',
+        img: 'images/product_list/pr.1.3.jpg',
+        price: 2,
+        discount: '10%'
+    },
+    {
+        id: 'pr4',
+        name: 'Медвежонок мягкая игрушка',
+        img: 'images/product_list/pr.1.4.jpg',
+        price: 6,
+    },
+    {
+        id: 'pr5',
+        name: 'Олененок мягкая игрушка',
+        img: 'images/product_list/pr.1.5.jpg',
+        price: 8.50,
+    },
+    {
+        id: 'pr6',
+        name: 'Елочное украшение',
+        img: 'images/product_list/pr.1.6.jpg',
+        price: 2,
+    },
+    {
+        id: 'pr7',
+        name: 'Венок зеленый',
+        img: 'images/product_list/pr.1.7.jpg',
+        price: 7,
+    },
+    {
+        id: 'pr8',
+        name: 'Венок синий',
+        img: 'images/product_list/pr.1.8.jpg',
+        price: 7,
+    },
+    {
+        id: 'pr9',
+        name: 'Венок красный',
+        img: 'images/product_list/pr.1.9.jpg',
+        price: 7,
+    },
+    {
+        id: 'pr10',
+        name: 'Подсвечник',
+        img: 'images/product_list/pr.1.10.jpg',
+        price: 9,
+    },
+    {
+        id: 'pr11',
+        name: 'Свеча красная',
+        img: 'images/product_list/pr.1.12.jpg',
+        price: 3.50,
+    },
+    {
+        id: 'pr14',
+        name: 'Нвбор кофе с маршмеллоу',
+        img: 'images/product_list/pr.1.14.png',
+        price: 3.50,
+        discount: '20%'
+    },
+    {
+        id: 'pr13',
+        name: 'Набор печенья',
+        img: 'images/product_list/pr.1.13.png',
+        price: 3.50,
+        discount: '15%'
+    },
+    {
+        id: 'pr15',
+        name: 'Чашка со снеговиком',
+        img: 'images/product_list/pr.1.15.png',
+        price: 5,
+        popular: true
+    },
+    {
+        id: 'pr16',
+        name: 'Носки утепленные',
+        img: 'images/product_list/pr.1.16.png',
+        price: 4,
+        popular: true
+    },
+    
+    
+];
